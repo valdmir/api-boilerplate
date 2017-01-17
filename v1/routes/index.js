b9eee000dc1be552f14c549004f2240385ad5e31
freddywijaya <vald.karate@gmail.com>
2017-01-17 10:55:16 +0700

    fixing

--- v1/routes/index.js
+++ v1/routes/index.js
@@ -1,16 +1,10 @@
 
 var routes= function(app){
   var general=require('./general')(app);
-  var packages=require('./packages')(app);
   var users=require('./users')(app);
-  var activity=require('./activity')(app);
-  var schedule=require('./schedule')(app);
 
   app.use("/api/v1",general);
   app.use("/api/v1",users);
-  app.use("/api/v1",packages);
-  app.use("/api/v1",activity);
-  app.use("/api/v1",schedule);
 
 
   return app;
